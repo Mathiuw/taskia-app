fb44623cc51d86887d96c001f6ba53018b54ee59
Mateus Sartori <98856859+Mathiuw@users.noreply.github.com>
2025-09-02 16:36:40 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.appContainer}>
+      <View style={styles.chatContainer}>
+        <Text>Chat starts here</Text>
+      </View>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder='Describe your task'/>
+        <Button title='send'/>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  chatContainer: {
+    flex: 6,
+    justifyContent: 'flex-end'
+  },
+
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+  }
+
 });
